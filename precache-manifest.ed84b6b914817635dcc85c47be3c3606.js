307e50816ae9e032cbe559113f9b1e46370271e9
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-04-10 04:07:41 -0400

    Updates

---
+++ precache-manifest.ed84b6b914817635dcc85c47be3c3606.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dc562c753e77649739f",
+    "url": "/desafioPinterest/static/css/main.134b6c2f.chunk.css"
+  },
+  {
+    "revision": "7dc562c753e77649739f",
+    "url": "/desafioPinterest/static/js/main.5bcd3345.chunk.js"
+  },
+  {
+    "revision": "1d7766f756632e966e1f",
+    "url": "/desafioPinterest/static/js/runtime~main.72bba9c9.js"
+  },
+  {
+    "revision": "a73d590eb9bf3f4b0c53",
+    "url": "/desafioPinterest/static/js/2.8d8b384c.chunk.js"
+  },
+  {
+    "revision": "841ef49141336a963b23631038976262",
+    "url": "/desafioPinterest/static/media/pinterest-logo.841ef491.png"
+  },
+  {
+    "revision": "e4fd33ea298f1b174cb4c727d4383cf1",
+    "url": "/desafioPinterest/static/media/lupa-vector-1.e4fd33ea.png"
+  },
+  {
+    "revision": "741f87fe9e442b1597339da5d5eb7553",
+    "url": "/desafioPinterest/static/media/Message_gray-512.741f87fe.png"
+  },
+  {
+    "revision": "ff5ea2f87c207ff32e384db3ccb546b0",
+    "url": "/desafioPinterest/index.html"
+  }
+];
