b4aeb2f2ed46c0178bdd8df1a68b0b9916d3fca0
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-04-10 04:05:10 -0400

    Replicando modal para deploy

--- src/Modal.js
+++ src/Modal.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import puntos from './img/puntos.png'
+import box from './img/outBox.png'
+import arrow from './img/arrow.png'
+import elefanta from './img/elefantita.jpg'
 import './modal.css'
 
 
@@ -10,11 +14,34 @@ function Modal (props) {
        }
    }}>
         <div className="divAdentro">
-        <div className="divImagen">
+        <div className="divNavbar">
+        <img src={puntos}   alt="icono" className="iconosModal"/>
+       
+        <img src={box} alt="icono" className="iconoBox"/>    
+        <span className="textoModal">Enviar</span>
+     
+        <span className="textModal">Categorías</span>
+        <img src={arrow} alt="icono" className="iconoArrow"/>
+        <span className="saveModal">Guardar</span>
+
+       </div>
+        <div className="divImagen">        
         <img src={props.item} alt="img" className="imgModal"/>
         </div>
         <div className="divTexto">
-        <p>holi holi holi</p>
+        <p  className="page"> Intagram.com</p>
+        <hr className="line" />
+        <p className="titulo">Fotos y comentarios</p>
+        <span className="select">Fotos</span> <span className="noSelect"> Comentarios</span>
+        <div>
+        <p className="parrafo">¿Has probado este Pin? Añade una foto para mostrar cómo ha ido</p>
+        <span className="plus">Añadir una <br/> foto</span>
+        <hr className="line" />
+        <img src={elefanta} alt="img" className="userModal"/>
+        <p className="parrafoUser"><strong>Heffalump</strong> a guardado este pin en <strong>Titanic</strong></p>
+        
+        </div>
+        
         </div>
 
         </div>
