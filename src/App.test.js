19e1e06082d4769573478544cc812a6a59dd2d1e
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-04-25 10:58:26 -0400

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('Se abre el modal y se muestra imagen',() => {
+  const modalrender = Enzyme.mount(<App/>)
+  const clickModal = modalrender.instance();
+  
+  clickModal.openModal("https://images.unsplash.com/photo-1526857240824-92be52581d9b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjY1NTU3fQ",
+)
+  
+  modalrender.update();
+  expect(modalrender.state().modal).toBe('https://images.unsplash.com/photo-1526857240824-92be52581d9b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjY1NTU3fQ')
+  
+  });
+  
+ 
